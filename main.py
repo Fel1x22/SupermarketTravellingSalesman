6a95ec63d207c3b661d6959c70589854c6391739
n72019fu <felix.underwood@student.manchester.ac.uk>
2025-06-11 13:03:44 +0100

    Working naive solution implemented

--- main.py
+++ main.py
@@ -47,6 +47,8 @@ basestart = [0, 6]
 
 baseitems = [[3, 2], [4, 2], [3, 11], [7, 10]]
 
+bigstart = [0, 19]
+
 #We can make assumptions about the layout of the store to make traversing much easier
 #So that we dont have to search to find an optimal route between items.
 
@@ -144,7 +146,7 @@ def findPath(itemA, itemB, store):
 
     if (abs(itemA[1]-itemB[1]) == 0 and (itemA[1] % 3 == 0)):
         #Take one step right, go down aisle then go one back left.
-        currentPos = itemA
+        currentPos = itemA.copy()
         steps += 2 + abs(itemA[0] - itemB[0])
 
         currentPos[1] = currentPos[1] + 1
@@ -165,11 +167,36 @@ def findPath(itemA, itemB, store):
 
         return steps, path
         #Currently just checking whether in top/bot rows. Should add check for adjacent cols as well.
-    elif (itemA[0] == 1) or (itemA[0] == 2) or (itemB[0] == 1) or (itemB[0] == 2) or (itemA[0] == len(store)-2) or (itemA[0] == len(store)-3) or (itemB[0] == len(store)-2) or (itemB[0] == len(store)-3)\
+    elif (itemA[0] == 1) or (itemA[0] == 2) or (itemA[0] == len(store)-2) or (itemA[0] == len(store)-3):
+
+        steps += abs(itemA[1] - itemB[1]) + abs(itemA[0] - itemB[0])
+        currentPos = itemA.copy()
+
+        horizontalSteps = abs(itemA[1] - itemB[1])
+        for i in range(1, horizontalSteps + 1):
+            if itemA[1] > itemB[1]:
+                currentPos[1] = currentPos[1] - 1
+                path.append([currentPos[0], currentPos[1]])
+            else:
+                currentPos[1] = currentPos[1] + 1
+                path.append([currentPos[0], currentPos[1]])
+
+        verticalSteps = abs(itemA[0] - itemB[0])
+        for i in range(1, verticalSteps+1):
+            if itemA[0] > itemB[0]:
+                currentPos[0] = currentPos[0] - 1
+                path.append([currentPos[0], currentPos[1]])
+            else:
+                currentPos[0] = currentPos[0] + 1
+                path.append([currentPos[0], currentPos[1]])
+
+        return steps, path
+
+    elif (itemB[0] == 1) or (itemB[0] == 2) or (itemB[0] == len(store)-2) or (itemB[0] == len(store)-3)\
             or (abs(itemA[1]-itemB[1]) == 1) or (abs(itemA[1]-itemB[1]) == 0):
 
         steps += abs(itemA[1] - itemB[1]) + abs(itemA[0] - itemB[0])
-        currentPos = itemA
+        currentPos = itemA.copy()
 
         verticalSteps = abs(itemA[0] - itemB[0])
         for i in range(1, verticalSteps+1):
@@ -195,13 +222,11 @@ def findPath(itemA, itemB, store):
 
         upperRoute = abs(2-itemA[0]) + abs(itemA[1] - itemB[1]) + abs(2-itemB[0])
         lowerRoute = abs((len(store)-3)-itemA[0]) + abs(itemA[1] - itemB[1]) + abs((len(store)-3)-itemB[0])
-        print(len(store))
-        print(upperRoute, lowerRoute)
 
         if upperRoute <= lowerRoute:
             steps = upperRoute
 
-            currentPos = itemA
+            currentPos = itemA.copy()
             verticalStepsOne = abs(2 - itemA[0])
             for i in range(1, verticalStepsOne + 1):
                 currentPos[0] = currentPos[0] - 1
@@ -218,14 +243,13 @@ def findPath(itemA, itemB, store):
 
             verticalStepsTwo = abs(2 - itemB[0])
             for i in range(1, verticalStepsTwo + 1):
-                print("hello")
                 currentPos[0] = currentPos[0] + 1
                 path.append([currentPos[0], currentPos[1]])
 
         else:
             steps = lowerRoute
 
-            currentPos = itemA
+            currentPos = itemA.copy()
             verticalStepsOne = abs((len(store)-3) - itemA[0])
             for i in range(1, verticalStepsOne + 1):
                 currentPos[0] = currentPos[0] + 1
@@ -248,27 +272,51 @@ def findPath(itemA, itemB, store):
 
         return steps, path
 
-
+#Naive solution that places items in the order they're listed. No thought about distances.
+#Looks like its working!
 def firstSolution(store, start, items):
-    stepsTaken = 0
-    finished = False
-    stepsTaken += abs(start[0] - items[0][0])
-    stepsTaken += abs(start[1] - items[0][1])
-    print(stepsTaken)
+    steps = 0
+
+
+    #Program runs into difficulty when dealing with the start zone, since its a row above. TO solve this, im gonna
+    #basically make the square in front the start/stop, then manually make the first/final steps.
+
+    fakeStart = [start[0]+1, start[1]]
+    finalpath = [fakeStart]
+    #print("Path at start", finalpath)
+
+    stops = [fakeStart]
+    stops.extend(items)
+    stops.append(fakeStart)
+
+
+
+    i = 0
+    while i < len(stops)-1:
+        """print("Stops:", stops)
+        print("i", i)
+        print("ItemA:", stops[i])
+        print("ItemB", stops[i+1])"""
+        tempSteps, tempPath = findPath(stops[i].copy(), stops[i+1].copy(), store)
+        #print(tempPath)
+        steps += tempSteps
+        #print("path before extending:", finalpath)
+        finalpath.extend(tempPath)
+        #print("path after: ", finalpath)
+        i = i + 1
+    finalpath.append(start)
+
+    return steps, finalpath
+
 
 
 #def bruteForce(store, items):
 
 
 #firstSolution(basestore, basestart, baseitems)
+testItem = [[1, 1]]
+printWithItems(bigbase, baseitems)
 
-"""steps, path = findPath([4, 2], [3, 11], basestore)
-print(steps)
-print(path)"""
 
-inputs = textListToCoordList(["1T", "1B"], bigbase)
-print(inputs)
 
-printWithItems(bigbase, inputs)
-
-print(findPath(inputs[0], inputs[1], bigbase))
+print(firstSolution(bigbase, bigstart, baseitems))
